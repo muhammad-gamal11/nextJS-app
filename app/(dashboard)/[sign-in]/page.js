58391484d049722fe1e06806bb585b7dcc19b6db
Muhammad Gamal <gamal9978@gmail.com>
2024-03-17 23:15:47 +0200

    added prisma

---
+++ app/(dashboard)/[sign-in]/page.js
@@ -0,0 +1,4 @@
+const signInPage = ({ params }) => {
+  return <h1 className="text-7xl">signInPage</h1>;
+};
+export default signInPage;
--- components/DrinksList.jsx
+++ components/DrinksList.jsx
@@ -1,12 +1,23 @@
 import Link from "next/link";
+import Image from "next/image";
+
 const DrinksList = ({ drinks }) => {
   return (
-    <ul className="menu menu-vertical pl-0">
+    <ul className="grid sm:grid-cols-2 gap-6 mt-6">
       {drinks.map((drink) => {
-        const { idDrink: id, strDrink: name } = drink;
+        const { idDrink: id, strDrink: name, strDrinkThumb: image } = drink;
         return (
           <li key={id}>
             <Link href={`/drinks/${id}`} className="text-xl font-medium">
+              <div className="relative h-48 mb-4">
+                <Image
+                  src={image}
+                  fill
+                  sizes="(max-width:768px) 100vw , (max-width:1200px) 50vw"
+                  alt={name}
+                  className="rounded-md object-cover"
+                />
+              </div>
               {name}
             </Link>
           </li>
