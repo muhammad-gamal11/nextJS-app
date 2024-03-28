2483065cf0a97358506ed0b6ab93ede35638fcfd
Muhammad Gamal <gamal9978@gmail.com>
2024-03-28 08:18:54 +0200

    providers added

--- utils/actions.js
+++ utils/actions.js
@@ -3,6 +3,7 @@
 import { redirect } from "next/navigation";
 import prisma from "../utils/db";
 import { revalidatePath } from "next/cache";
+import { z } from "zod";
 
 const getAllTasks = async () => {
   return await prisma.task.findMany({
@@ -24,17 +25,21 @@ export const createTask = async (formData) => {
 };
 
 export const createTaskCustom = async (prevState, formData) => {
-  // await new Promise((resolve) => setTimeout(resolve, 1000));
   const content = formData.get("content");
+  const Task = z.object({
+    content: z.string().min(5),
+  });
   try {
+    Task.parse({ content });
     await prisma.task.create({
       data: {
         content,
       },
     });
     revalidatePath("/tasks");
-    return { message: "success !!!" };
+    return { message: "success" };
   } catch (error) {
+    console.log(error);
     return { message: "error..." };
   }
 };
